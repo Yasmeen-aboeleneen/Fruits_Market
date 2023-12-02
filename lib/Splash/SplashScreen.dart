b4382a964b91ff3dfee9a08b95c554c79aa05b49
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2023-12-02 23:17:17 +0200

    Start in Splash Screen

---
+++ lib/Splash/SplashScreen.dart
@@ -0,0 +1,16 @@
+import 'package:e_commerce_app/Splash/SplashScreenBody.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const SplashScreenBody();
+  }
+}
---
+++ lib/Splash/SplashScreenBody.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenBody extends StatefulWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
