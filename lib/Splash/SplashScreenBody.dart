564ec99a48ea1c30180f41a71a682c1efd59f10b
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2023-12-14 23:55:01 +0200

    Add Animation to Splash Screen

--- lib/Splash/SplashScreenBody.dart
+++ lib/Splash/SplashScreenBody.dart
@@ -7,19 +7,43 @@ class SplashScreenBody extends StatefulWidget {
   State<SplashScreenBody> createState() => _SplashScreenBodyState();
 }
 
-class _SplashScreenBodyState extends State<SplashScreenBody> {
+class _SplashScreenBodyState extends State<SplashScreenBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+
+    animationController?.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
           const Spacer(),
-          const Text.rich(TextSpan(
-              text: 'Fruits Market',
-              style: TextStyle(
-                  fontSize: 50,
-                  fontFamily: 'IBMPlexSans-Bold',
-                  color: Color.fromARGB(255, 228, 228, 228)))),
+          FadeTransition(
+            opacity: fadingAnimation!,
+            child: const Text.rich(TextSpan(
+                text: 'Fruits Market',
+                style: TextStyle(
+                    fontSize: 50,
+                    fontFamily: 'IBMPlexSans-Bold',
+                    color: Color.fromARGB(255, 228, 228, 228)))),
+          ),
           Image.asset('Assets/Images/splash_view_image.png'),
         ],
       ),
